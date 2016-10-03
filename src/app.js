db0311c5483ccb903753bb187a93c07b5d6c3d60
Tangui Clairet <Tangui_CLAIRET>
2016-10-03 11:24:14 +0200

    Changement du code

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-var http = require('http')
-var user = require('./user.js')
+var http = require('http');
+var user = require('./user.js');
 
-http.creatServer(function (req, res)
+http.createServer(function (req, res)
 {
-  user.get("cesar", function(id)
+  user.get("Tangui", function(id)
   {
     res.writeHead(200,{'Content-Type': 'text/plain'})
     res.end("hello " + id);
   })
-  
+
 }).listen(1337,'127.0.0.1');
