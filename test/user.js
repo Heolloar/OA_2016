ca618f1f0977df870e1f0d44cf12201e9527d3ca
Tangui Clairet <Tangui_CLAIRET>
2016-10-10 09:42:52 +0200

    gitignore

--- test/user.js
+++ test/user.js
@@ -1,1 +1,16 @@
-car should = require('should');
+var should = require('should');
+var user = require('../tp1/src/user.js');
+
+describe('my test series', function() {
+  it('get : should return tangui', function(){
+    user.get("tangui",function(res){
+      res.should.equal("tangui");
+    })
+  })
+  it('save : should return tangui', function(){
+    user.save("tangui",function(res){
+      res.should.equal("tangui");
+    })
+  })
+  //it('should do smth else', function(done){...})
+})
